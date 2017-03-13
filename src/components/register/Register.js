b2fbd9bea1213fb81caf8f04da39b6f3dbfdab6c
Cheri <avyishi@gmail.com>
2017-03-12 22:46:59 -0400

    Fixed issue with register and registerform

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import RegisterForm from './RegisterForm'
 export default class Register extends Component {
 	render(){
 		return (
 			<div>
-				<h1 className="text-center">Register Here!</h1>
+				<h1 className="text-center">Register!</h1>
+				<RegisterForm />
 			</div>
 		);
 	}	
