596671c2ec4bf0ec3afef1dc2ff597e74a0e2fae
Cheri <avyishi@gmail.com>
2017-03-17 21:26:44 -0400

    Added submit button function

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -11,15 +11,30 @@ export default class RegisterForm extends Component {
 		}
 
 		this.onChange = this.onChange.bind(this);
+		this._submit = this._submit.bind(this);
 	}
 
 	onChange(e){
 		this.setState({ [e.target.name]: e.target.value});
 	}
 
+	_submit(e){
+		e.preventDefault()
+		let { username, email, password1, password2 } = this.state;
+
+		if(password1 !== password2) {
+			this.setState({
+				password1: '',
+				password2: '' 
+			})
+			return alert("Passwords do not match")
+		}
+	}
+
 	render(){
+		let { username, email, password1, password2 } = this.state;
 		return (
-			<form>
+			<form onSubmit={this._submit}>
 				<h1>Join Us!</h1>
 				<div>
 					<label>Username</label>
@@ -48,7 +63,7 @@ export default class RegisterForm extends Component {
 					/>
 				</div>
 				<div>
-					<button className="btn btn-primary">
+					<button type="submit" className="btn btn-primary">
 					Sign up
 					</button>
 				</div>
